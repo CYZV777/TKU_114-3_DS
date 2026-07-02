3feb5e7c65774f3275a9b30f52f898e3b36af916
Yi-Chen Chung <btsarmymaggie0910@gmail.com>
2026-07-03 00:26:49 +0800

    Add score report homework

--- 0702/ScoreReport.java
+++ 0702/ScoreReport.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ScoreReport {
     public static void main(String[] args) {
-
+    // 讀取使用者輸入的姓名與三科成績，並計算印出平均分數
         Scanner sc = new Scanner(System.in);
 
         System.out.print("請輸入姓名：");
@@ -17,6 +17,7 @@ public class ScoreReport {
         System.out.print("請輸入 Math 成績：");
         int mathScore = sc.nextInt();
 
+        // 計算平均分數
         double average = (javaScore + englishScore + mathScore) / 3.0;
 
         System.out.println("\n=== 成績報表 ===");
@@ -25,6 +26,7 @@ public class ScoreReport {
         System.out.println("English：" + englishScore);
         System.out.println("Math：" + mathScore);
         
+        // 輸出計算後的平均分數
         System.out.println("平均：" + average);
 
         sc.close();
